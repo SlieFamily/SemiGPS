c6f2b937770a25d1598b5c9997c483d65e4aecac
SlieFamily <sliewdyinwhite@gmail.com>
2024-08-03 16:39:49 +0800

    update svi_feat

--- datasets/gba_graph.py
+++ datasets/gba_graph.py
@@ -5,10 +5,22 @@ import numpy as np
 import pandas as pd
 
 class GbaDataset(Dataset):
-    def __init__(self, root="datasets/", transform=None, pre_transform=None):
+    def __init__(self, root="datasets/", feature='all', transform=None, pre_transform=None):
+        """
+        Args:
+            feature (string): select type of feature ('all', 'poi' or 'svi').
+        """
         super().__init__(root, transform, pre_transform)
-
-        self.node_feature = torch.load(root+"poi_feature.pt").float()
+        
+        if feature == 'poi':
+            self.node_feature = torch.load(root+"poi_feature.pt").float()
+        elif feature == 'svi':
+            self.node_feature = torch.load(root+"svi_feature.pt").float()
+        else:
+            poi = torch.load(root+"poi_feature.pt").float()
+            svi = torch.load(root+"svi_feature.pt").float()
+            self.node_feature = torch.cat([svi, poi], dim=1).shape
+        
         self.move = pd.read_csv(root+"gba_move_cnt.csv", index_col=0)
         self.gdp = pd.read_csv(root+"gba_3gdp.csv", index_col=0)
 
